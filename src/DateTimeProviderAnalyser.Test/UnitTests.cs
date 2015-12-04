919fceb92c7f93410f0ab82fc56591c8c401c601
Dennis Roche <droche@abngroup.com.au>
2015-12-04 15:41:45 +0800

    Added and then refactored the Roslyn Analyser template provided by the VSSDK

---
+++ src/DateTimeProviderAnalyser.Test/UnitTests.cs
@@ -0,0 +1,61 @@
+﻿using DateTimeProviderAnalyser.Test.TestHelpers;
+using Microsoft.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DateTimeProviderAnalyser.Test
+{
+    [TestClass]
+    public class UnitTests
+    {
+        public UnitTests()
+        {
+            _diagnosticVerifier = new DateTimeProviderDiagnosticVerifier();
+            _fixVerifier = new DateTimeProviderCodeFixVerifier();
+        }
+
+        [TestMethod]
+        public void ExpectNoDiagnosticResults()
+        {
+            const string source = @"";
+            _diagnosticVerifier.VerifyCSharpDiagnostic(source);
+        }
+
+        [TestMethod]
+        public void ApplySuggestedFix()
+        {
+            const string sourceCodeWithIssue = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+        }
+    }";
+
+            var expected = new DiagnosticResult
+            {
+                Id = "DateTimeProviderAnalyser",
+                Message = "Type name \'TypeName\' contains lowercase letters",
+                Severity = DiagnosticSeverity.Warning,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 6, 15) }
+            };
+
+            _diagnosticVerifier.VerifyCSharpDiagnostic(sourceCodeWithIssue, expected);
+
+            const string sourceWithCodeFix = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TYPENAME
+        {
+        }
+    }";
+            _fixVerifier.VerifyCSharpFix(sourceCodeWithIssue, sourceWithCodeFix);
+        }
+
+        private readonly DateTimeProviderCodeFixVerifier _fixVerifier;
+        private readonly DateTimeProviderDiagnosticVerifier _diagnosticVerifier;
+    }
+}
---
+++ src/DateTimeProviderAnalyser/Scripts/Uninstall.ps1
@@ -0,0 +1,34 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+function Remove-AnalyserFromProject {
+    param (
+        $ToolsPath,
+        $Project,
+        $LanguageFolder
+    )
+
+    $analyzersPaths = Join-Path (Join-Path (Split-Path -Path $ToolsPath -Parent) "analyzers" ) * -Resolve
+
+    foreach($path in $analyzersPaths)
+    {
+        $path = Join-Path $path -ChildPath $LanguageFolder -Resolve
+
+        if (Test-Path $path)
+        {
+            foreach ($analyzerFilePath in Get-ChildItem $path -Filter *.dll) {
+                try {
+                    if($Project.Object.AnalyzerReferences) {
+                        $Project.Object.AnalyzerReferences.Remove($analyzerFilePath.FullName)
+                    }
+                } catch {
+                    Write-Warning "Error trying to remove AnalyzerReference: '$_'"
+                }
+            }
+        }
+    }
+}
+
+Remove-AnalyserFromProject -ToolsPath $ToolsPath -LanguageFolder ""
+if($Project.Type -eq "C#") {
+    Remove-AnalyserFromProject -ToolsPath $ToolsPath -LanguageFolder "cs"
+}
